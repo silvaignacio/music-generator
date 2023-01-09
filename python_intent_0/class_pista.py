6cbcaeff93c74fd90819ea05d387bed7bdc6f6ad
Rene Castillo S <rcastillosi@bancofalabella.cl>
2023-01-09 17:32:14 -0300

    Ordering and definition

---
+++ python_intent_0/class_pista.py
@@ -0,0 +1,10 @@
+print("class pista")
+
+#por def la pista tendrá 4 * 3 instrumentos posibles
+
+#cada uno de esos instrumentos tendrá 4 variaciones
+#esto es una matriz de 4*4*3 pposibles casillas a teclear
+
+#un instrumento pude ser de efectos, en ese caso no deben tienen para que estar relacionados
+#en promedio se usarán 8 efectos
+
